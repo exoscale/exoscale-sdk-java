1931cd546db6408294c268817ebfa034acb4daa6
elkezza <salih.elkezza@gmail.com>
2024-04-11 12:34:05 +0200

    Implementing single Client class extending ExoscaleApi with credentials and wait functionality, matching

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,6 @@
 import org.openapitools.client.ApiClient;
 import org.openapitools.client.ApiException;
+import org.openapitools.client.Client;
 import org.openapitools.client.Credentials;
 import org.openapitools.client.api.ExoscaleApi;
 import org.openapitools.client.model.*;
@@ -10,19 +11,19 @@ public class Main {
 
     public static void main(String[] args) throws ApiException, InterruptedException {
 
-        ApiClient apiClient = new ApiClient(new Credentials("Put your Secret Key", "Put your Public Key"));
-        ExoscaleApi exoscaleApi = new ExoscaleApi(apiClient);
-        InstanceType instanceType = exoscaleApi.getInstanceType(UUID.fromString("b6e9d1e8-89fc-4db3-aaa4-9b4c5b1d0844"));
+        Client client = new Client(new Credentials("Put your Secret Key", "Put your Public Key"));
+
+        InstanceType instanceType = client.getInstanceType(UUID.fromString("b6e9d1e8-89fc-4db3-aaa4-9b4c5b1d0844"));
         System.out.println(instanceType.toString());
-        Template template = exoscaleApi.getTemplate(UUID.fromString("4e4d4f64-9608-4dc3-b5f4-979c8ac39e7f"));
+        Template template = client.getTemplate(UUID.fromString("4e4d4f64-9608-4dc3-b5f4-979c8ac39e7f"));
         CreateInstanceRequest request = new CreateInstanceRequest()
                 .instanceType(instanceType)
                 .template(template)
                 .diskSize(50L)
                 .publicIpAssignment(PublicIpAssignment.INET4);
-        Operation operation = exoscaleApi.createInstance(request);
+        Operation operation = client.createInstance(request);
         while(Operation.StateEnum.PENDING.equals(operation.getState())) {
-            operation = exoscaleApi.getOperation(operation.getId());
+            operation = client.getOperation(operation.getId());
             Thread.sleep(1000);
         }
     }
