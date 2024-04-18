b20d4c53a60757def4b3bb723c685c4dd55a551b
elkezza <salih.elkezza@gmail.com>
2024-04-18 16:16:03 +0200

    Adding exoscale configuration into openapi-generator plugin in order to organise the code, fixing the static classes within the custom template.

--- libraries/native/Client.java
+++ libraries/native/Client.java
@@ -1,8 +1,9 @@
-package com.exoscale.client;
+package com.exoscale.exoscale4j.client;
 
 
-import com.exoscale.api.ExoscaleApi;
-import com.exoscale.model.Operation;
+import com.exoscale.exoscale4j.api.*;
+import com.exoscale.exoscale4j.model.*;
+import com.exoscale.exoscale4j.client.*;
 
 /**
 * High-level wrapper for interacting with Exoscale API.
@@ -14,9 +15,7 @@ public class Client extends ExoscaleApi {
 * @param credentials The credentials used to authenticate with the Exoscale API.
 */
 
-public Client(Credentials credentials){
-super(new ApiClient(credentials));
-}
+public Client(Credentials credentials){ super(new ApiClient(credentials)); }
 
 /**
 * Waits for a given operation to complete.
@@ -26,15 +25,15 @@ super(new ApiClient(credentials));
 * @throws RuntimeException If there is an error during the polling process
 */
 public Operation wait(Operation operation){
-while(Operation.StateEnum.PENDING.equals(operation.getState())) {
 
-try {
-operation = this.getOperation(operation.getId());
-Thread.sleep(1000);
-} catch (Exception e) {
-throw new RuntimeException(e);
-}
-}
-return operation;
-}
+    while(Operation.StateEnum.PENDING.equals(operation.getState())) {
+        try {
+                operation = this.getOperation(operation.getId());
+                Thread.sleep(1000);
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+    return operation;
+    }
 }
--- libraries/native/Credentials.java
+++ libraries/native/Credentials.java
@@ -1,4 +1,4 @@
-package com.exoscale.client;
+package com.exoscale.exoscale4j.client;
 
 import javax.crypto.Mac;
 import javax.crypto.spec.SecretKeySpec;
@@ -16,22 +16,21 @@ private static final String COMMA = ",";
 private String secretKey;
 private String apiKey;
 public Credentials(String apiKey, String secretKey) {
-this.apiKey = apiKey;
-this.secretKey = secretKey;
+    this.apiKey = apiKey;
+    this.secretKey = secretKey;
 }
 
 public String generateSignature(String method, String path, String requestBody) throws Exception {
-long unixTimestamp = System.currentTimeMillis() / 1000L;
-String message = method + SPACE + path + LINE_BREAK+LINE_BREAK+LINE_BREAK+LINE_BREAK + unixTimestamp;
-if (!requestBody.isEmpty()) {
-message = method + SPACE + path + LINE_BREAK + requestBody + LINE_BREAK+LINE_BREAK+LINE_BREAK + unixTimestamp;
+    long unixTimestamp = System.currentTimeMillis() / 1000L;
+    String message = method + SPACE + path + LINE_BREAK+LINE_BREAK+LINE_BREAK+LINE_BREAK + unixTimestamp;
+    if (!requestBody.isEmpty()) {
+        message = method + SPACE + path + LINE_BREAK + requestBody + LINE_BREAK+LINE_BREAK+LINE_BREAK + unixTimestamp;
+    }
+    Mac mac = Mac.getInstance(HMAC_SHA_256);
+    SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), HMAC_SHA_256);
+    mac.init(secretKeySpec);
+    byte[] hash = mac.doFinal(message.getBytes());
+    String signature = Base64.getEncoder().encodeToString(hash);
+    return EXO_2_HMAC_SHA_256_CREDENTIAL + apiKey + COMMA + EXPIRES + unixTimestamp + COMMA + SIGNATURE + signature;
+    }
 }
-Mac mac = Mac.getInstance(HMAC_SHA_256);
-SecretKeySpec secretKeySpec = new SecretKeySpec(secretKey.getBytes(), HMAC_SHA_256);
-mac.init(secretKeySpec);
-byte[] hash = mac.doFinal(message.getBytes());
-String signature = Base64.getEncoder().encodeToString(hash);
-return EXO_2_HMAC_SHA_256_CREDENTIAL + apiKey + COMMA + EXPIRES + unixTimestamp + COMMA + SIGNATURE + signature;
-}
-}
-
