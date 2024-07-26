15471703761c2337e0cf98fc4f5d25463f74517e
elkezza <47871727+elkezza@users.noreply.github.com>
2024-07-26 17:04:37 +0200

    Add Javadoc files for GitHub Pages

--- CHANGELOG.md
+++
@@ -1,6 +0,0 @@
-# Changelog
-
-## [0.0.1-ALPHA] - 2024-06-10
-
-### Added
-- Initial release.
---
+++ javadoc/0.0.1-ALPHA/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.exoscale.exoscale4j.api"},{"l":"com.exoscale.exoscale4j.client"},{"l":"com.exoscale.exoscale4j.model"}];updateSearchResults();
