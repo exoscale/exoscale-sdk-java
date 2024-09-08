64c19beef840e2287c74a05390cfd27bf71194a9
Automated <actions@users.noreply.github.com>
2024-09-08 09:59:50 +0000

    OpenAPI spec and SDK update: Sun Sep  8 09:59:50 UTC 2024

--- sdk/src/main/java/com/exoscale/sdk/client/Configuration.java
+++ sdk/src/main/java/com/exoscale/sdk/client/Configuration.java
@@ -15,7 +15,7 @@ package com.exoscale.sdk.client;
 
 @javax.annotation.Generated(value = "org.openapitools.codegen.languages.JavaClientCodegen", comments = "Generator version: 7.4.0")
 public class Configuration {
-    public static final String VERSION = "0.0.6-ALPHA";
+    public static final String VERSION = "0.0.7-ALPHA";
 
     private static ApiClient defaultApiClient = new ApiClient();
 
