b093287b579448787e08e0d1aa4ea6333f28cb79
Automated <actions@users.noreply.github.com>
2024-09-09 09:56:07 +0000

    Update JavaDocs for 0.0.8-ALPHA

--- javadoc/current/0.0.2-ALPHA/package-search-index.js
+++
@@ -1,1 +0,0 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.exoscale.sdk.api"},{"l":"com.exoscale.sdk.client"},{"l":"com.exoscale.sdk.model"}];updateSearchResults();
