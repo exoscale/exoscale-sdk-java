7cd638c7d4c98ddd7f710c7f6ee68c9e45818a2b
Exoscale <operation+build@exoscale.net>
2025-01-18 07:06:20 +0000

    Update JavaDocs for 0.0.5-SNAPSHOT-f260706

--- javadoc/current/package-search-index.js
+++ javadoc/current/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","url":"allpackages-index.html"},{"l":"com.exoscale.sdk.api"},{"l":"com.exoscale.sdk.client"},{"l":"com.exoscale.sdk.model"}]
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.exoscale.sdk.api"},{"l":"com.exoscale.sdk.client"},{"l":"com.exoscale.sdk.model"}];updateSearchResults();
